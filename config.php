2285ed00bf7cd5073b938539744f752d9deed013
james <james@africaloop.com>
2018-01-25 16:02:26 +0000

    pgsql setup

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ session_start();
 $servername = "ec2-54-235-220-220.compute-1.amazonaws.com";
 $username = "tqcldbjguvavxn";
 $password = "d044dc5d51b0c73f3558f79494e3a3070793b1a30479ed2b85da0aa49659bf7f";
-$dbname = "kindle";
+$dbname = "d1todkffj2lu9q";
 
 try {
     $conn = new PDO("pgsql:host=$servername;dbname=$dbname", $username, $password);
