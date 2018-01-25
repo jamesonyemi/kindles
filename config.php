b5ff3688921dab4eb9fd89cfbec95333e1b1be47
james <james@africaloop.com>
2018-01-25 16:52:12 +0000

    pgsql settup

--- config.php
+++ config.php
@@ -1,12 +1,13 @@
 <?php
 session_start();
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "kindle";
+$servername = "ec2-54-235-220-220.compute-1.amazonaws.com";
+$username = "tqcldbjguvavxn";
+$password = "d044dc5d51b0c73f3558f79494e3a3070793b1a30479ed2b85da0aa49659bf7f";
+$dbname = "d1todkffj2lu9q";
+$port	= "5432";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn = new PDO("pgsql:host=$servername;dbname=$dbname", $username, $password, $port);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
    }
