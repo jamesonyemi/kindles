14a75c2e984d9c2c7d16242e88c67cf944320c66
james <james@africaloop.com>
2018-01-25 16:00:11 +0000

    pgsql on heroku

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "ec2-54-235-220-220.compute-1.amazonaws.com";
+$username = "tqcldbjguvavxn";
+$password = "d044dc5d51b0c73f3558f79494e3a3070793b1a30479ed2b85da0aa49659bf7f";
 $dbname = "kindle";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn = new PDO("pgsql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
    }
